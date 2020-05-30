1d5c9c55080d4f198d8e3ec97c134aacd6156a6a
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-30 13:43:07 -0400

    Add click handler

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      board:Array(9).fill(null)
+    }
+  }
+
+  handleClick = (index) => {
+    console.log(index)
+    
+    let newBoard =[...this.state.board]
+    newBoard[index] = "X"
+
+    this.setState({
+      board: newBoard
+    })
+  }
+
     render() {
+      const Box = this.state.board.map((box,index) => <div className="box" onClick={() =>this.handleClick(index)} key={index}>{box}</div>)
         return (
             <div className="container">
                 <h1>
                     Tic Tac Toe App
                 </h1>
                 <div className="board">
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
+                  {Box}
                 </div>
             </div>
         );
